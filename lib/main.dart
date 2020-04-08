35736746be522df51a3653aa226e767ad3c9d826
LuaFly <luasoestudos@yahoo.com>
2020-04-08 10:27:29 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+      title: "Contador de Gatos",
+      home: Home() ));
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _cat = 0;
+  String _infoText = "Quantos Gatos";
+
+  void _changeCat(int delta){
+    setState(() {
+      _cat +=delta;
+
+      if(_cat <= 0 ){
+        _infoText = "Você odeia gatos";
+      }
+      else if (_cat >0 && _cat <4){
+        _infoText= "Você gosta de gatos";
+      }
+      else{
+        _infoText= "A louca dos gatos";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/cat.jpg",
+          fit: BoxFit.fill,
+          height: 1000,
+        ),
+        Column(
+          //coluna
+          mainAxisAlignment:
+          MainAxisAlignment.center, //alinhar coluna, vertical
+          children: <Widget>[
+            //childre: quantos filhos quiser
+            Text(
+              "Gatos: $_cat",
+              style: TextStyle(
+                  color: Colors.greenAccent, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment:
+              MainAxisAlignment.center, //alinhar linha horizontal
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0), //parametro
+                  child: FlatButton(
+                    //child: um filho só
+                    child: Text(
+                      "+1",
+                      style:
+                      TextStyle(fontSize: 40.0, color: Colors.deepPurple),
+                    ),
+                    onPressed: () {
+                      _changeCat(1);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style:
+                      TextStyle(fontSize: 40.0, color: Colors.deepPurple),
+                    ),
+                    onPressed: () {
+                      _changeCat(-1);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.greenAccent,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
